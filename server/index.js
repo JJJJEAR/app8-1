dfdf80707c05c7ac74c3204b84399e1d78c40296
JJJJEAR <s6303051623152@email.kmutnb.ac.th>
2023-10-13 22:35:04 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,20 +29,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(express.static('public'))
 
-// app.get('/api/form-get', (request, response) => {
-//     let t = request.query.target || ''
-//     let k = request.query.kw || ''
-//     let n = parseInt((Math.random() * 1000))
-//     let r = {
-//         target: t,
-//         kw: k,
-//         results: n
-//     }
-//     response.json(r)
-// })
-// app.listen(port, () => {
-//     console.log('Server listening on port ' + port)
-// })
+app.get('/api/form-get', (request, response) => {
+    let t = request.query.target || ''
+    let k = request.query.kw || ''
+    let n = parseInt((Math.random() * 1000))
+    let r = {
+        target: t,
+        kw: k,
+        results: n
+    }
+    response.json(r)
+})
 
 app.post('/api/form-post', (request, response) => {
     let name = request.body.name || '';
